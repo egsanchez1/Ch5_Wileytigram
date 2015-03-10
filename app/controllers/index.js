a9c8e2e46e56cdc2383efa8e557e5b12df64d131
egsanchez1 <egsanchez1@buffs.wtamu.edu>
2015-03-09 22:48:37 -0500

    Initial Commit

---
+++ app/controllers/index.js
@@ -0,0 +1,18 @@
+// when we start up, create a user and log in
+var user = Alloy.createModel('User');
+
+// we are using the default administration account for now
+user.login("wileytigram_admin", "wileytigram_admin", function(_response) {
+	
+	if(_response.success)
+	{
+		//$.index.open();
+		$.tabGroup.open();
+
+		// pre-populate the feed with recent photos
+		$.feedController.initialize();		
+	} else {
+  		alert("Error starting application " + _response.error);
+  		Ti.API.error('error logging in ' + _response.error);
+	}
+});
